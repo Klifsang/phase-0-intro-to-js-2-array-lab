1377a5040eb07692e4c5368e133b3b0803241a7b
Klif <sang.wicklife@student.moringaschool.com>
2023-12-18 10:19:20 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = (name)=>{
+    return cats.push(name);
+}
+
+const destructivelyPrependCat = (name)=>{
+    return cats.unshift(name);
+}
+
+const destructivelyRemoveLastCat = ()=>{
+    return cats.pop();
+}
+
+const destructivelyRemoveFirstCat = () =>{
+    return cats.shift();
+}
+
+const appendCat = (name)=>{
+    return [...cats,name];
+}
+
+const prependCat = (name)=>{
+    return [name,...cats]
+}
+
+const removeLastCat = ()=>{
+    let newCatsArr = [...cats];
+    newCatsArr.pop();
+    return newCatsArr;
+}
+
+const removeFirstCat = () =>{
+    let newCatsArr = [...cats];
+    newCatsArr.shift();
+    return newCatsArr;
+}
